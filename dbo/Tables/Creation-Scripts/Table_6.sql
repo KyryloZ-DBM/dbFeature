5e99cee486e0277e5483032df246f32b7cae8a11
KyryloZ-DBM <82807333+KyryloZ-DBM@users.noreply.github.com>
2022-03-03 12:40:35 +0200

    Update Table_6.sql

--- dbo/Tables/Creation-Scripts/Table_6.sql
+++ dbo/Tables/Creation-Scripts/Table_6.sql
@@ -5,7 +5,7 @@ GO
 CREATE TABLE [dbo].[Table_6](
 	[col1] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col2] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-	[col3] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[diffColName] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 	[col5] [nchar](10) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 )
 
